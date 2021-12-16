e01a3481e92f00103f814fc13ca536188a66c347
antony <antony.jacques06@gmail.com>
2021-12-16 19:05:35 +0100

    error404

--- src/View/Error404.js
+++ src/View/Error404.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import { Link } from 'react-router-dom';
+
+import '../css/Error404.css';
  
 class Error404 extends React.Component {
     render() {
         return (
-            <div>Error404</div>
+            <div id="error404">
+                <Header location={window.location.pathname} />
+                <main>
+                    <div className='main_message'>404</div>
+                    <div className='second_message'>Oups! La page que vous demandez n'existe pas.</div>
+                    <div className='back_home'>
+                        <Link to="/">Retourner sur la page d’accueil</Link>
+                    </div>
+                </main>
+                <Footer />
+            </div>
         )
     }
 }
