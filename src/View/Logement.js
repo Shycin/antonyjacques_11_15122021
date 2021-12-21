105e0177b8dd4ea6a98e9c96334ee08b7524de32
antony <antony.jacques06@gmail.com>
2021-12-21 16:58:45 +0100

    logment description auteur

--- src/View/Logement.js
+++ src/View/Logement.js
@@ -4,6 +4,7 @@ import Header from '../Components/Header';
 import Slides from '../Components/Slides';
 import Footer from '../Components/Footer';
 
+import {star} from '../data/picto.js';
 import dataAPI from '../data/appart.json';
 
 import '../css/Logement.css';
@@ -19,13 +20,46 @@ class Logement extends React.Component {
         };  
     }
 
+    star(max, score)
+    {
+        var elements = [];
+        for(let i = 0; i < max; i++){
+            elements.push(<div className={`${ i < score ? 'rateActive' : '' } `}>{star}</div>);
+        }
+        return elements;
+    }
+
     render() {
         return (
             <div id="home">
                 <Header location={window.location.pathname} />
                 <main>
                     <Slides images={this.state.appart.pictures}/>
-                    {this.state.appart.id}
+                    <div className='flex_container description'>
+                        <div className='left'>
+                            <h1 id="title">{this.state.appart.title}</h1>
+                            <div id="location">{this.state.appart.location}</div>
+                            <div id="tags">
+                                {
+                                    this.state.appart.tags.map( (tag, index) => {
+                                        return (<div className='tag' key={index}>{tag}</div>)
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <div className='right'>
+                            <div id="author">
+                                <h2 id="authorName">{this.state.appart.host.name.split(' ')[0]}<br/>{this.state.appart.host.name.split(' ')[1]}</h2>
+                                <div id="authorPicture"><img src={this.state.appart.host.picture} alt="auteur"/></div>
+                            </div>
+                            <div id="rating">
+                                {
+                                    this.star(5,this.state.appart.rating)
+                                }
+                            </div>
+                        </div>
+                    </div>
+
                 </main>
                 <Footer />
             </div>
